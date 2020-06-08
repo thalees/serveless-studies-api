ac41dd82736ec2e2f97639a0219af9257d0d4099
Leonardo Leme <leonardo.leme@brap.com.br>
2020-06-08 16:38:12 -0300

    created the Podcast DAO interface

--- src/main/java/br/com/studiesMaterials/dao/PodcastDao.java
+++ src/main/java/br/com/studiesMaterials/dao/PodcastDao.java
@@ -1,4 +1,12 @@
 package br.com.studiesMaterials.dao;
 
+import br.com.studiesMaterials.web.api.schemas.PodcastPostSchema;
+
+import java.util.UUID;
+
 public interface PodcastDao {
+    String findAll();
+    void create(PodcastPostSchema paramSchema);
+    void update(PodcastPostSchema pramSchema);
+    void delete(UUID podcastId);
 }
