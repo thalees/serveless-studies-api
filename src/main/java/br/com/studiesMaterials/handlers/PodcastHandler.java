2dbd89fb09cddb8307c764a548f32fd83f891e28
Leonardo Leme <leonardo.leme@brap.com.br>
2020-06-08 17:04:52 -0300

    created the Podcast Handler with the findAll method

---
+++ src/main/java/br/com/studiesMaterials/handlers/PodcastHandler.java
@@ -0,0 +1,68 @@
+package br.com.studiesMaterials.handlers;
+
+import br.com.studiesMaterials.dao.PodcastDao;
+import br.com.studiesMaterials.db.DataBase;
+import br.com.studiesMaterials.domain.Book;
+import br.com.studiesMaterials.domain.Podcast;
+import br.com.studiesMaterials.web.api.schemas.PodcastPostSchema;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PodcastHandler implements PodcastDao{
+
+    @Override
+    public String findAll() {
+        final List<Podcast> podcasts = new ArrayList<>();
+
+        try(Connection conn = DataBase.connection()) {
+            assert conn != null;
+            Statement statement = conn.createStatement();
+
+            ResultSet rs = statement.executeQuery(
+                    "SELECT * FROM public.podcast"
+            );
+
+            while (rs.next()) {
+                Podcast podcast = new Podcast(
+                        rs.getString("id"),
+                        rs.getString("student_id"),
+                        rs.getString("subject"),
+                        rs.getInt("time"),
+                        rs.getString("link")
+                );
+                podcasts.add(podcast);
+            }
+
+            return serializerResponse(podcasts);
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void create(PodcastPostSchema paramSchema) {
+
+    }
+
+    @Override
+    public void update(PodcastPostSchema pramSchema) {
+
+    }
+
+    @Override
+    public void delete(UUID podcastId) {
+
+    }
+
+    private String serializerResponse(List<Podcast> podcasts) {
+        Gson gson = new Gson();
+        ObjectMapper mapper = new ObjectMapper();
+        return gson.toJson(podcasts);
+    }
+}
