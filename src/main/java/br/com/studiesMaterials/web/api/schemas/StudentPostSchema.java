9a9212c2e2d58f4f1c2420e20f99284b18fd896d
Leonardo Leme <leonardo3morais7@gmail.com>
2020-06-08 18:46:36 -0300

    created the student schema

---
+++ src/main/java/br/com/studiesMaterials/web/api/schemas/StudentPostSchema.java
@@ -0,0 +1,10 @@
+package br.com.studiesMaterials.web.api.schemas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentPostSchema {
+    public String username;
+}
