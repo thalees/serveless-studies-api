ded75c5b0ca92fa0aeea9a16ecc262b17ab44794
Leonardo Leme <leonardo.leme@brap.com.br>
2020-06-08 16:33:35 -0300

    created the Podcast schema

---
+++ src/main/java/br/com/studiesMaterials/dao/PodcastDao.java
@@ -0,0 +1,4 @@
+package br.com.studiesMaterials.dao;
+
+public interface PodcastDao {
+}
---
+++ src/main/java/br/com/studiesMaterials/web/api/schemas/PodcastPostSchema.java
@@ -0,0 +1,15 @@
+package br.com.studiesMaterials.web.api.schemas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class PodcastPostSchema {
+    public UUID user_id;
+    public String subject;
+    public int time;
+    public String link;
+}
