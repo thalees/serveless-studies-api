3eb6cd314da9ff2ef140b950075eccd7719263d3
Thales Pereira <thalespereiradejesus@gmail.com>
2020-06-10 19:54:01 -0300

    Fix methods in BookHandler

--- src/main/java/br/com/studiesMaterials/dao/BookDao.java
+++ src/main/java/br/com/studiesMaterials/dao/BookDao.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 public interface BookDao {
-    String findAll(APIGatewayProxyRequestEvent input);
     APIGatewayProxyResponseEvent create(APIGatewayProxyRequestEvent input);
     APIGatewayProxyResponseEvent update(APIGatewayProxyRequestEvent input);
     APIGatewayProxyResponseEvent delete(APIGatewayProxyRequestEvent input);
--- src/main/java/br/com/studiesMaterials/handlers/BookHandler.java
+++ src/main/java/br/com/studiesMaterials/handlers/BookHandler.java
@@ -2,50 +2,14 @@ package br.com.studiesMaterials.handlers;
 
 import br.com.studiesMaterials.dao.BookDao;
 import br.com.studiesMaterials.db.DataBase;
-import br.com.studiesMaterials.domain.Book;
 import br.com.studiesMaterials.web.api.schemas.BookSchema;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BookHandler implements BookDao {
-    @Override
-    public String findAll(APIGatewayProxyRequestEvent input) {
-        final List<Book> books = new ArrayList<>();
-
-        try(Connection conn = DataBase.connection()) {
-            assert conn != null;
-            String studentId =  input.getPathParameters().get("id");
-
-            Statement statement = conn.createStatement();
-            String sql = String.format("SELECT * FROM public.book WHERE student_id = '%s'", studentId);
-            ResultSet rs = statement.executeQuery(sql);
-            conn.close();
-
-            while (rs.next()) {
-                Book book = new Book(
-                    rs.getString("id"),
-                    rs.getString("student_id"),
-                    rs.getString("subject"),
-                    rs.getString("title"),
-                    rs.getString("author"),
-                    rs.getString("link")
-                );
-                books.add(book);
-            }
-
-
-            return serializerResponse(books);
-        } catch (SQLException error) {
-            error.printStackTrace();
-        }
-        return null;
-    }
-
     @Override
     public APIGatewayProxyResponseEvent create(APIGatewayProxyRequestEvent input) {
         APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
@@ -53,15 +17,16 @@ public class BookHandler implements BookDao {
             assert conn != null;
             Statement statement = conn.createStatement();
 
-            String studentId =  input.getPathParameters().get("id");
+            String studentId =  input.getPathParameters().get("student_id");
 
             BookSchema data = convertBody(input.getBody());
 
             String sql = String.format(
                 "INSERT INTO public.book (student_id, subject, title, author, link) " +
-                "VALUES ('%s', '%s', '%s', '%s')", studentId, data.subject, data.title, data.author, data.link
+                "VALUES ('%s', '%s', '%s', '%s', '%s')", studentId, data.subject, data.title, data.author, data.link
             );
-            statement.executeQuery(sql);
+
+            statement.executeUpdate(sql);
 
             responseEvent.setStatusCode(201);
 
@@ -111,11 +76,11 @@ public class BookHandler implements BookDao {
         try(Connection conn = DataBase.connection()) {
             assert conn != null;
             Statement statement = conn.createStatement();
-            String id = input.getPathParameters().get("id");
+            String bookId = input.getPathParameters().get("book_id");
 
-            String sql = String.format("DELETE FROM public.book WHERE id = '%s'", id);
+            String sql = String.format("DELETE FROM public.book WHERE id = '%s'", bookId);
 
-            statement.executeQuery(sql);
+            statement.executeUpdate(sql);
 
             responseEvent.setStatusCode(204);
 
@@ -129,11 +94,6 @@ public class BookHandler implements BookDao {
 
             return responseEvent;
         }
-    }
-
-    private String serializerResponse(List<Book> books) {
-        Gson gson = new Gson();
-        return gson.toJson(books);
     }
 
     private BookSchema convertBody(String json){
